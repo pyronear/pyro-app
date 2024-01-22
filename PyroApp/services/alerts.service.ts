6885b70fc2c9c28c2b3acb50a91ab0d8fb2860cd
Camille Boire <camille.boire@student-cs.fr>
2024-01-22 22:51:46 +0100

    Add constant for cleaner code

--- PyroApp/services/alerts.service.ts
+++ PyroApp/services/alerts.service.ts
@@ -2,6 +2,7 @@ import apiClient from './apiClient.service';
 
 const CAMERA_OPENING_ANGLE = 87;
 const CAMERA_RANGE_KM = 15;
+const CONVERTION_KM_TO_DEG = 111.0;
 
 type alert = {
   id: number;
@@ -46,16 +47,18 @@ function calculateCoordinatesTriangle(alert: alert): number[][] {
   const dist_lng_1 = CAMERA_RANGE_KM * Math.sin(azimuth1);
 
   // calcul des coordonnées en degrés d'une extrémité du triangle
-  const lat1 = alert.lat + dist_lat_1 / 111.0;
-  const lng1 = alert.lon + dist_lng_1 / 111.0 / Math.cos(latitude);
+  const lat1 = alert.lat + dist_lat_1 / CONVERTION_KM_TO_DEG;
+  const lng1 =
+    alert.lon + dist_lng_1 / CONVERTION_KM_TO_DEG / Math.cos(latitude);
 
   // calcul des coordonées en km d'une extrémité du triangle par rapport au centre
   const dist_lat_2 = CAMERA_RANGE_KM * Math.cos(azimuth2);
   const dist_lng_2 = CAMERA_RANGE_KM * Math.sin(azimuth2);
 
   // calcul des coordonnées en degrés d'une extrémité du triangle
-  const lat2 = alert.lat + dist_lat_2 / 111.0;
-  const lng2 = alert.lon + dist_lng_2 / 111.0 / Math.cos(latitude);
+  const lat2 = alert.lat + dist_lat_2 / CONVERTION_KM_TO_DEG;
+  const lng2 =
+    alert.lon + dist_lng_2 / CONVERTION_KM_TO_DEG / Math.cos(latitude);
 
   return [
     [lat1, lng1],
