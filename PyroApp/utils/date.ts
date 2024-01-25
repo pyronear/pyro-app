d320f370d4c8736630cf0ab8d83e6013368fcd26
Camille Boire <camille.boire@student-cs.fr>
2024-01-25 11:29:58 +0100

    Improve date formatting

--- PyroApp/utils/date.ts
+++ PyroApp/utils/date.ts
@@ -1,14 +1,7 @@
 export function convertFormatDate(dateString: string): string {
   const dateObj = new Date(dateString);
 
-  const jour = dateObj.getDate().toString().padStart(2, '0');
-  const mois = (dateObj.getMonth() + 1).toString().padStart(2, '0');
-  const annee = dateObj.getFullYear();
-
-  const heures = dateObj.getHours().toString().padStart(2, '0');
-  const minutes = dateObj.getMinutes().toString().padStart(2, '0');
-
-  const dateFormated = `${jour}/${mois}/${annee} ${heures}:${minutes}`;
+  const dateFormated = dateObj.toLocaleString('fr-FR');
 
   return dateFormated;
 }
